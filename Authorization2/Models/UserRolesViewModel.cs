812fe64fa03b6953a15e09f7b817f5caf34908d4
MakSimKlim <83maxomus@mail.ru>
2025-04-30 10:26:32 +0500

    Initial commit (новый старт)

---
+++ Authorization2/Models/UserRolesViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Authorization2.Models
+{
+    public class UserRolesViewModel
+    {
+        public string UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<string> Roles { get; set; }
+    }
+}
